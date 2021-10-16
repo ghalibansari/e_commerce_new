e5f6291c9fe2b31821866faac8ba6d2b88994706
dev <ghalibansari1994@gmail.com>
2021-10-16 13:59:43 +0530

    rewrited validate function

--- src/app/helper/Validation.ts
+++ src/app/helper/Validation.ts
@@ -3,45 +3,22 @@ import { JsonResponse } from "./JsonResponse";
 import Joi from "joi";
 
 
-async function check(type: string): Promise<any> {
-    if (type === 'body') return async function (req: Request, res: Response, next: NextFunction, schema: any) {
-        schema.validateAsync(req.body, { abortEarly: false }).then(() => next())
-            .catch(async (err: any) => { res.locals = { status: false, message: err.message }; await JsonResponse.jsonSuccess(req, res); })
-    }
-
-    if (type === 'query') return async function (req: Request, res: Response, next: NextFunction, schema: any) {
-        schema.validateAsync(req.query, { abortEarly: false }).then(() => next())
-            .catch(async (err: any) => { res.locals = { status: false, message: err.message }; await JsonResponse.jsonSuccess(req, res); })
-    }
-
-    if (type === 'bodyQuery') return async function (req: Request, res: Response, next: NextFunction, schema: any) {
-        schema.validateAsync({ ...req.body, ...req.query }, { abortEarly: false }).then(() => next())
-            .catch(async (err: any) => { res.locals = { status: false, message: err.message }; await JsonResponse.jsonSuccess(req, res); })
-    }
-}
-
-//FIXME: remove any type.
-const validateBody = (schema: any) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        await schema.validateAsync(req.body)
-            .then(() => { next() })
-            .catch(async (err: Error) => {
-                res.locals = { status: false, message: err.message };
-                await JsonResponse.jsonSuccess(req, res);
-            });
+const validate = (key: 'body'|'params'|'query') => {
+    //FIXME: remove any type.
+    return (schema: any) => {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            await schema
+                .validateAsync(req[key])
+                .then(() => { next() })
+                .catch(async (err: Error) => {
+                    res.locals = { status: false, message: err.message };
+                    await JsonResponse.jsonSuccess(req, res);
+                });
+        };
     };
 };
 
-//FIXME: remove any type.
-const validateParams = (schema: any) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        await schema.validateAsync(req.params)
-            .then(() => { next() })
-            .catch(async (err: Error) => {
-                res.locals = { status: false, message: err.message };
-                await JsonResponse.jsonSuccess(req, res);
-            });
-    };
-};
+const validateBody = validate('body')
+const validateParams = validate('params')
 
 export { validateBody, validateParams };
