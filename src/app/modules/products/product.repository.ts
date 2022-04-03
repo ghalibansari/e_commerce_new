d5625219bf357d6cc5d4c672c39629a8f7ac29b6
Ghalib Ansari <ghalibansari1994@gmail.com>
2022-04-03 18:15:16 +0530

    feat
    similar products added remaning key

--- src/app/modules/products/product.repository.ts
+++ src/app/modules/products/product.repository.ts
@@ -1,7 +1,11 @@
 
 import { DB } from "../../../configs/DB";
 import { BaseRepository } from "../BaseRepository";
+import { BrandMd } from "../brand/brand.model";
+import { CategoriesMd } from "../categories/categories.model";
 import { ICategories } from "../categories/categories.type";
+import { ProductImagesMd } from "../product-images/product-images.model";
+import { UnitMasterMd } from "../unit-master/unit-master.model";
 import { ProductMd } from "./product.model";
 import { IMProduct, IProduct } from "./product.type";
 
@@ -14,8 +18,35 @@ export class ProductRepository extends BaseRepository<IProduct, IMProduct> {
     }
 
     similarRandomProducts = async ({ category_id, limit }: { category_id: ICategories['category_id'], limit: number }) => {
+        const attributes = ["name", "description", "selling_price", "weight", 'out_of_stock', 'base_price'];
+        const include = [
+            {
+                model: BrandMd,
+                as: "brand",
+                attributes: ["brand_name", "brand_id"],
+                where: { is_active: true },
+            },
+            {
+                model: CategoriesMd,
+                as: "category",
+                attributes: ["category_name", "category_id"],
+                where: { is_active: true },
+            },
+            {
+                model: ProductImagesMd,
+                as: "images",
+                attributes: ["image_url"],
+                where: { is_active: true },
+                limit: 1
+            },
+            {
+                model: UnitMasterMd,
+                as: "unit",
+                attributes: ["name"]
+            }
+        ];
         //@ts-expect-error
-        return await this.findBulkBR({ where: { category_id }, order: [literal('RANDOM()'), literal('RANDOM()')], limit, attributes: ['product_id', 'category_id', "name"] })//
+        return await this.findBulkBR({ where: { category_id }, order: [literal('RANDOM()'), literal('RANDOM()')], limit, attributes, include })//
     };
 
 };
